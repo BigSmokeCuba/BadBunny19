1a7c1e55a398349d47f213db9fddcc322a9e2081
BigSmokeCuba <101540930+BigSmokeCuba@users.noreply.github.com>
2022-04-03 04:55:13 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 BOT_TOKEN = '5151062359:AAE2pcB0S312wuehcIG2Joeo1Yw4cZbY_gs' #token de tu bot
 API_ID = 13193029 # lo creas en https://my.telegram.org/
 API_HASH = '03bca249fdbe6824b1ca6d928a2e60ee' # lo creas en https://my.telegram.org/
-OWNER = [BigSmoke19]
+OWNER = BigSmoke19
 
 # DATOS DEL MOODLE
 USUARIO = 'amtoj18000'
